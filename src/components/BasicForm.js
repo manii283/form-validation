0d23880793775acb5f30b607ae2f88428cbced83
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-06-23 15:45:07 +0530

    form submission and getting usr input values

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,3 +1,5 @@
+
+
 const BasicForm = (props) => {
     return (
       <form>
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,16 +1,37 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
-    return (
-      <form>
-        <div className='form-control'>
-          <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className="form-actions">
-          <button>Submit</button>
-        </div>
-      </form>
-    );
+    const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangedHandler = (event) => {
+    setEnteredName(event.target.value);
   };
-  
-  export default SimpleInput;
-  
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName('');
+  };
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangedHandler}
+          ref={nameInputRef}
+          value={enteredName}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
